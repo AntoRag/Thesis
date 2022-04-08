92c571e46cb909ad85b96e66566bf5f3beceaf93
Antonio Ragazzo <antonio.ragazzo@outlook.it>
2022-04-08 12:27:59 +0200

    Adding the Catkin Workspace and the first test_node

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/antonio/Thesis/src
+# Build directory: /home/antonio/Thesis/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("test_node")
