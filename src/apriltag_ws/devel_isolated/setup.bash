33d3f776f7cf6ee621adca3efff56dfcd72c67ba
Antonio Ragazzo <antonio.ragazzo@outlook.it>
2022-04-29 12:52:50 +0200

    Update

--- src/apriltag_ws/devel_isolated/setup.bash
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# generated from catkin.builder Python module
-
-. "/home/antonio/apriltag_ws/devel_isolated/apriltag_ros/setup.bash"
--- src/apriltag_ws/devel_isolated/setup.zsh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env zsh
-# generated from catkin.builder Python module
-
-. "/home/antonio/apriltag_ws/devel_isolated/apriltag_ros/setup.zsh"
