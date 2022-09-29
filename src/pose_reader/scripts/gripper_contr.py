d9810e8649db2a9d06bbab5fa2a2aa21e1fcb4bd
Antonio Ragazzo <antonio.ragazzo@outlook.it>
2022-09-29 18:04:51 +0200

    Fix

--- src/pose_reader/scripts/gripper_contr.py
+++ src/pose_reader/scripts/gripper_contr.py
@@ -9,10 +9,6 @@ from bondpy import bondpy
 import rospy
 import moveit_commander  
 import moveit_msgs.msg
-from geometry_msgs.msg import PoseStamped
-from geometry_msgs.msg import Pose
-import actionlib
-from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 
 from std_msgs.msg import String
 from moveit_commander.conversions import pose_to_list
@@ -20,11 +16,12 @@ from moveit_commander.conversions import pose_to_list
 
 
 def gripper_callback(data):
-    bond = bondpy.Bond("/locobot/status_gripper", id)
-    bond.start()
+    bond1 = bondpy.Bond("/locobot/open_gripper","opengripper")
+    bond2 = bondpy.Bond("/locobot/close_gripper","closegripper")
     gripper_name = "interbotix_gripper"
     move_group_gripper = moveit_commander.MoveGroupCommander(gripper_name,robot_description="locobot/robot_description")
     if data.data == 'Open':
+        bond1.start()
         joint_goal = move_group_gripper.get_current_joint_values()
         print("joint status",joint_goal[0]," ",joint_goal[1])
         joint_goal[0] = 0.035
@@ -37,25 +34,29 @@ def gripper_callback(data):
         move_group_gripper.stop()
         ## END_SUB_TUTORIAL
         # For testing:
+        bond1.break_bond()
     elif data.data == 'Close':
-        joint_goal = move_group_gripper.get_current_joint_values()
-        rospy.loginfo("joint status",joint_goal[0]," ",joint_goal[1])
-        joint_goal[0] = 0.01
-        joint_goal[1] = -0.01
+        bond2.start()
+        joint_goal_close = move_group_gripper.get_current_joint_values()
+        print("joint status",joint_goal_close[0]," ",joint_goal_close[1])
+        joint_goal_close[0] = 0.015
+        joint_goal_close[1] = -0.015
         # The go command can be called with joint values, poses, or without any
         # parameters if you have already set the pose or joint target for the group
-        move_group_gripper.go(joint_goal, wait=True)
+        move_group_gripper.go(joint_goal_close, wait=True)
         rospy.loginfo("Gripper Closed")
         # Calling ``stop()`` ensures that there is no residual movement
         move_group_gripper.stop()
         ## END_SUB_TUTORIAL
         # For testing:
-    bond.break_bond()
+        bond2.break_bond()
+    
 
 def listener():
 
     rospy.init_node('gripper_controller')
     rospy.Subscriber("/locobot/gripper_command", String , gripper_callback)
+
     rospy.spin()
 
 
--- src/pose_reader/scripts/movement_node.py
+++ src/pose_reader/scripts/movement_node.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
+from lib2to3.pgen2.tokenize import generate_tokens
 import sys
 import copy
 import os
 os.environ['ROS_NAMESPACE'] = 'locobot'
 # Must set `os.environ['ROS_NAMESPACE']` BEFORE importing `rospy`
 from bondpy import bondpy
+import uuid
 import rospy
 import moveit_commander  
 import moveit_msgs.msg
@@ -16,7 +18,7 @@ from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 
 from std_msgs.msg import String
 from moveit_commander.conversions import pose_to_list
-
+from bondpy import bondpy
 try:
     from math import pi, tau, dist, fabs, cos
 except:  # For Python 2 compatibility
@@ -27,8 +29,6 @@ except:  # For Python 2 compatibility
     def dist(p, q):
         return sqrt(sum((p_i - q_i) ** 2.0 for p_i, q_i in zip(p, q)))
 
-
-id = bondpy.generate_unique_id()
 
 def add_box(target_pose,scene):
     # Copy class variables to local variables to make the web tutorials more clear.
@@ -44,21 +44,30 @@ def add_box(target_pose,scene):
     box_pose.header.frame_id = "locobot/base_footprint"
     box_pose.pose.orientation.w = 1.0
     box_pose.pose.position.z = target_pose.pose.position.z   # above the panda_hand frame
-    box_pose.pose.position.x = target_pose.pose.position.x-0.03 
-    box_pose.pose.position.y = target_pose.pose.position.y
-    scene.add_box(box_name, box_pose, size=(0.06, 0.06, 0.06))
+    box_pose.pose.position.x = 0.4*target_pose.pose.position.x+0.03 
+    box_pose.pose.position.y = 0.4*target_pose.pose.position.y
+    scene.add_box(box_name, box_pose, size=(0.06, 0.04, 0.06))
     rospy.sleep(5)
 
-
-def gripper_status_callback(status):
-    global gripper_status_bool
-    if status.data == 'Finished':
-        gripper_status_bool = 0
-    elif status.data == 'Moving':
-        gripper_status_bool = 1
-        
+def attach_box(robot,scene,move_group):
+    box_name = 'medicine'
+    eef_link = move_group.get_end_effector_link()
+    grasping_group = 'interbotix_gripper'
+    touch_links = robot.get_link_names(group=grasping_group)
+    scene.attach_box(eef_link, box_name, touch_links=touch_links)
+    rospy.sleep(5)
+    return
     
+def detach_box(scene,move_group):
+    box_name = 'medicine'
+    eef_link = move_group.get_end_effector_link()
+    scene.remove_attached_object(eef_link, name=box_name)
+    return
 
+def remove_box(scene):
+    box_name = 'medicine'
+    scene.remove_world_object(box_name)
+    return
 
 def go_to_pose_goal(move_group,target_pose):
     # Copy class variables to local variables to make the web tutorials more clear.
@@ -73,8 +82,8 @@ def go_to_pose_goal(move_group,target_pose):
     ## end-effector:
     pose_goal = Pose()
     pose_goal.orientation.w = 1
-    pose_goal.position.x = 0.4*target_pose.pose.position.x
-    pose_goal.position.y = 0.4*target_pose.pose.position.y
+    pose_goal.position.x = 0.45*target_pose.pose.position.x
+    pose_goal.position.y = 0.45*target_pose.pose.position.y
     pose_goal.position.z = target_pose.pose.position.z
     rospy.loginfo("Setting target pose")
     rospy.sleep(1)
@@ -133,7 +142,7 @@ def move_base_client(target):
     goal.target_pose.pose.position.y = 0.6*target.pose.position.y
     goal.target_pose.pose.orientation.z = r2 * z1 + x2 * y1 - y2 * x1 + z2 * r1
     goal.target_pose.pose.orientation.w = r2 * r1 - x2 * x1 - y2 * y1 - z2 * z1
-    
+    print("Target position",goal.target_pose.pose.position.x," ",goal.target_pose.pose.position.y)
     client.send_goal(goal)
     wait = client.wait_for_result()
     if not wait:
@@ -160,35 +169,37 @@ def movement_callback(pose_goal):
     print("Robot currently running")
     
     moveit_commander.roscpp_initialize(sys.argv)
-    #robot = moveit_commander.RobotCommander(robot_description="/locobot/robot_description")
+    robot = moveit_commander.RobotCommander(robot_description="/locobot/robot_description")
     scene = moveit_commander.PlanningSceneInterface()
     arm_name = "interbotix_arm"
     move_group_arm = moveit_commander.MoveGroupCommander(arm_name,robot_description="locobot/robot_description")
-    bond = bondpy.Bond("/locobot/status_gripper", id)
-    
+    bond1 = bondpy.Bond("/locobot/open_gripper","opengripper")
+    bond2 = bondpy.Bond("/locobot/close_gripper","closegripper")
     move_base_client(pose_goal)
 
-    add_box(pose_goal,scene)
+    # add_box(pose_goal,scene)
     pre_grasp_pose = pose_goal
-    pre_grasp_pose.pose.position.x = pre_grasp_pose.pose.position.x - 0.05
+    pre_grasp_pose.pose.position.x = pre_grasp_pose.pose.position.x - 0.03
     go_to_pose_goal(move_group_arm,pre_grasp_pose)
     
-    gripper_command.publish("Open")
-    bond.start()
-    if not bond.wait_until_formed(rospy.Duration(1.0)):
+    gripper_command.publish('Open')
+    bond1.start()
+    if not bond1.wait_until_formed(rospy.Duration(10.0)):
         raise Exception('Bond could not be formed')
-    bond.wait_until_broken()
+    bond1.wait_until_broken()
     rospy.loginfo("Gripped Opened")
     
     go_to_pose_goal(move_group_arm,pose_goal)
 
-    gripper_command.publish("Close")
-    bond.start()
-    if not bond.wait_until_formed(rospy.Duration(1.0)):
+    gripper_command.publish('Close')
+    bond2.start()
+    if not bond2.wait_until_formed(rospy.Duration(10.0)):
         raise Exception('Bond could not be formed')
-    bond.wait_until_broken()
+    bond2.wait_until_broken()
     rospy.loginfo("Gripped Closed")
     
+    attach_box(robot, scene, move_group_arm)
+
     
     current_status = "idle"
     robot_status_pub.publish(current_status)
@@ -200,7 +211,7 @@ def listener():
 
     rospy.init_node('movement_node')
     rospy.Subscriber("/locobot/pose_goal", PoseStamped , movement_callback)
-    rospy.Subscriber("/locobot/gripper_status", String , gripper_status_callback)
+    # rospy.Subscriber("/locobot/gripper_status", String , gripper_status_callback)
     rospy.spin()
 
 
