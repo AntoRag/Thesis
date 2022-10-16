f9c73c827f0e41ecb510fd2b9cd631d62596c0d6
Federico-Maresca <fmare94@gmail.com>
2022-10-16 15:12:14 +0200

    deleted costmap manager

--- src/core/src/base_controller.cpp
+++ src/core/src/base_controller.cpp
@@ -41,31 +41,22 @@ nav_msgs::GetPlan srv;
 bool wait;
 bool success_navigation;
 
-// #include <std_msgs/Bool.h>
-// ros::Publisher pub_goal_request;
-// bool isGoalOccupied;
-
-// void goalOccupiedCallback(std_msgs::Bool rAnswer)
-//     {
-//     isGoalOccupied = rAnswer.data;
-//     }
-
 // Handles simple case of goal pose from Communication Manacer
-bool moveBaseGoalCalc(move_base_msgs::MoveBaseGoal &pGoal)
-{
+bool moveBaseGoalCalc(move_base_msgs::MoveBaseGoal& pGoal)
+    {
     srv.request.goal = pGoal.target_pose;
     check_goal_feasable.call(srv);
     if (srv.response.plan.poses.size() != 0)
-    {
+        {
         ROS_INFO("[CORE::BASE_CONTROLLER] ---- GOAL NOT OCCUPIED! Trying ");
         return true;
-    }
+        }
     ROS_WARN("[CORE::BASE_CONTROLLER] ---- GOAL OCCUPIED! Trying new position");
     return false;
-}
+    }
 
 void moveBaseCallback(geometry_msgs::PoseStamped pPose)
-{
+    {
     move_base_msgs::MoveBaseGoal rGoal;
     ROS_INFO("[CORE::BASE_CONTROLLER] ---- Inside movebasecallback");
     rGoal.target_pose.pose.position = pPose.pose.position;
@@ -74,43 +65,43 @@ void moveBaseCallback(geometry_msgs::PoseStamped pPose)
 
     MoveBaseClient moveBaseClient("/locobot/move_base", true);
     if (moveBaseGoalCalc(rGoal))
-    {
-        while (!moveBaseClient.waitForServer(ros::Duration(5.0)))
         {
+        while (!moveBaseClient.waitForServer(ros::Duration(5.0)))
+            {
             ROS_INFO("[CORE::BASE_CONTROLLER] ---- Waiting for the move_base action server to come up");
-        }
+            }
         base_status_msg.data = BASE_TO_GOAL;
         pub_status.publish(base_status_msg);
         moveBaseClient.sendGoal(rGoal);
         moveBaseClient.waitForResult();
         auto rState = moveBaseClient.getState();
         if (moveBaseClient.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-        {
+            {
             ROS_INFO("[CORE::BASE_CONTROLLER] ---- Goal execution done!");
             base_status_msg.data = BASE_GOAL_OK;
             pub_status.publish(base_status_msg);
-        }
+            }
         else
-        {
+            {
             ROS_INFO("[CORE::BASE_CONTROLLER] ---- Goal not reached!");
             base_status_msg.data = BASE_GOAL_FAIL;
             pub_status.publish(base_status_msg);
+            }
         }
-    }
     else
-    {
+        {
         ROS_INFO("[CORE::BASE_CONTROLLER] ---- Goal not reached!");
         base_status_msg.data = BASE_GOAL_FAIL;
         pub_status.publish(base_status_msg);
-    }
+        }
     ::sleep(5);
     base_status_msg.data = BASE_IDLE;
     pub_status.publish(base_status_msg);
-}
+    }
 
-int main(int argc, char **argv)
-{
-    putenv((char *)"ROS_NAMESPACE=locobot");
+int main(int argc, char** argv)
+    {
+    putenv((char*)"ROS_NAMESPACE=locobot");
     ros::init(argc, argv, "base_controller");
     ros::NodeHandle node_handle;
     ros::Subscriber sub_mobile_goal_pose = node_handle.subscribe("/locobot/frodo/mobile_pose_goal", 1, moveBaseCallback);
@@ -119,4 +110,4 @@ int main(int argc, char **argv)
     check_goal_feasable = node_handle.serviceClient<nav_msgs::GetPlan>("/locobot/move_base/make_plan");
 
     ros::spin();
-}
+    }
